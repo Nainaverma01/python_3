fd9f39e18d25628f1c43871ba9d9ace3d8826631
Nainaverma01 <98697671+Nainaverma01@users.noreply.github.com>
2023-05-25 15:43:19 +0530

    Create if_else.py
    This tells you about if else conditionals

---
+++ rollercoaster/if_else.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+age = int (input("What is your age? "))
+if height >= 120 :
+  print ("you can ride the rollercoaster")
+else :
+  print ("Sorry, You need to get taller to ride")
